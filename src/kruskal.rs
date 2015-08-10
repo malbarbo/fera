592c59349e0639b069456b0ae9629c332b91446e
Marco A L Barbosa <malbarbo@gmail.com>
2015-08-10 16:51:50 -0300

    Add UnionFind structure and Kruskal mst algorithm.

---
+++ src/kruskal.rs
@@ -0,0 +1,78 @@
+use super::*;
+use super::unionfind::DisjointSet;
+
+pub trait Visitor<G: Basic> {
+    fn visit(&mut self, e: G::Edge, in_same_set: bool) -> bool;
+}
+
+impl<F, G> Visitor< G> for F
+    where G: Basic,
+          F: FnMut(G::Edge, bool) -> bool {
+    fn visit(&mut self, e: G::Edge, in_same_set: bool) -> bool {
+        self(e, in_same_set)
+    }
+}
+
+pub trait Kruskal: Basic + WithEdgeProp + WithVertexProp + Sized {
+    fn kruskal_edges<I, V>(&self, edges: I, mut visitor: V)
+        where I: Iterator<Item=Self::Edge>,
+              V: Visitor<Self>,
+              Self::Vertex: PartialEq {
+        let mut ds = DisjointSet::new(self);
+        for e in edges {
+            let (u, v) = self.edge_vertices(e);
+            let in_same_set = ds.in_same_set(u, v);
+            if !visitor.visit(e, in_same_set) {
+                return;
+            }
+            if !in_same_set {
+                ds.union(u, v);
+            }
+        }
+    }
+
+    fn kruskal<T, V>(&self, weight: &EdgeProp<Self, T>, visitor: V)
+        where T: Ord,
+              V: Visitor<Self>,
+              Self: for<'a> EdgePropType<'a, T>,
+              Self::Vertex: PartialEq {
+        let mut edges = self.edges().collect::<Vec<_>>();
+        edges.sort_by(|a, b| weight[*a].cmp(&weight[*b]));
+        self.kruskal_edges(edges.iter().cloned(), visitor);
+    }
+
+    fn kruskal_mst<T>(&self, weight: &EdgeProp<Self, T>) -> Vec<Self::Edge>
+        where T: Ord,
+              Self: for<'a> EdgePropType<'a, T>,
+              Self::Vertex: PartialEq {
+        let mut tree = vec![];
+        self.kruskal::<T, _>(weight, |e: Self::Edge, in_same_set: bool| {
+            if !in_same_set {
+                tree.push(e);
+            }
+            tree.len() != self.num_vertices() - 1
+        });
+        tree
+    }
+}
+
+impl<G> Kruskal for G where G: Basic + WithVertexProp + WithEdgeProp { }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::super::*;
+    use super::super::graphadj::StaticGraph;
+
+    #[test]
+    fn kruskal_mst() {
+        let g = StaticGraph::new_edges(5, &[(0, 4), (2, 3), (0, 1), (1, 4), (1, 2), (2, 4), (3, 4)]);
+        // expected tree                      0       1       2               3
+        let mut weight = g.edge_prop(0usize);
+        for (e, w) in g.edges().zip(&[1, 2, 3, 4, 5, 6, 7]) {
+            weight[e] = *w;
+        }
+        assert_eq!(vec![0, 1, 2, 4], g.kruskal_mst(&weight));
+    }
+}
---
+++ src/unionfind.rs
@@ -0,0 +1,82 @@
+use super::*;
+
+pub struct DisjointSet<'a, G: Basic + WithVertexProp> {
+    parent: VertexProp<'a, G, G::Vertex>,
+    rank: VertexProp<'a, G, usize>
+}
+
+impl<'a, G> DisjointSet<'a, G>
+    where G: Basic + WithVertexProp,
+          G::Vertex: PartialEq {
+    pub fn new(g: &G) -> DisjointSet<G> {
+        let mut ds = DisjointSet::<G> {
+            parent: g.vertex_prop(g.vertices().next().unwrap()),
+            rank: g.vertex_prop(0usize)
+        };
+
+        for v in g.vertices() {
+            ds.parent[v] = v;
+        }
+
+        ds
+    }
+
+    pub fn union(&mut self, x: G::Vertex, y: G::Vertex) {
+        let a = self.find_set(x);
+        let b = self.find_set(y);
+        assert!( a != b );
+        self.link(a, b);
+    }
+
+    pub fn in_same_set(&mut self, x: G::Vertex, y: G::Vertex) -> bool {
+        self.find_set(x) == self.find_set(y)
+    }
+
+    fn link(&mut self, x: G::Vertex, y: G::Vertex) {
+        if self.rank[x] > self.rank[y] {
+            self.parent[y] = x;
+        } else {
+            self.parent[x] = y;
+            if self.rank[x] == self.rank[y] {
+                self.rank[y] += 1
+            }
+        }
+    }
+
+    fn find_set(&mut self, x: G::Vertex) -> G::Vertex {
+        // TODO: write a iterative version
+        if self.parent[x] != x {
+            let p = self.parent[x];
+            self.parent[x] = self.find_set(p);
+        }
+        self.parent[x]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::super::graphadj::StaticGraph;
+
+    fn check_groups(ds: &mut DisjointSet<StaticGraph>, groups: &[&[usize]]) {
+        for group in groups.iter() {
+            for a in group.iter() {
+                assert!(ds.in_same_set(group[0], *a));
+            }
+        }
+    }
+
+    #[test]
+    fn unionfind() {
+        let g = StaticGraph::new(5, vec![], vec![]);
+        let mut ds = DisjointSet::new(&g);
+        ds.union(0, 2);
+        check_groups(&mut ds, &[&[0, 2]]);
+        ds.union(1, 3);
+        check_groups(&mut ds, &[&[0, 2], &[1, 3]]);
+        ds.union(2, 4);
+        check_groups(&mut ds, &[&[0, 2, 4], &[1, 3]]);
+        ds.union(3, 4);
+        check_groups(&mut ds, &[&[0, 2, 4, 1, 3]]);
+    }
+}
