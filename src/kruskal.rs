f0e87d6d43187a96811a2745fb13253ae62f24c8
Marco A L Barbosa <malbarbo@gmail.com>
2015-08-26 14:30:01 -0300

    Simplify Kruskal visitor.
    - Create Accept enum to make it clear when the edge is accepted.

--- src/kruskal.rs
+++ src/kruskal.rs
@@ -1,43 +1,51 @@
 use graph::*;
+use iter::IteratorExt;
 use unionfind::DisjointSet;
 
+#[derive(PartialEq, Eq)]
+pub enum Accept {
+    Yes,
+    No,
+}
+
 pub trait Visitor<'a, G: Basic<'a>> {
-    fn visit(&mut self, e: G::Edge, in_same_set: bool) -> bool;
+    fn visit(&mut self, e: G::Edge) -> Accept;
 }
 
 impl<'a, F, G> Visitor<'a, G> for F
     where G: Basic<'a>,
-          F: FnMut(G::Edge, bool) -> bool {
-    fn visit(&mut self, e: G::Edge, in_same_set: bool) -> bool {
-        self(e, in_same_set)
+          F: FnMut(G::Edge) -> Accept {
+    fn visit(&mut self, e: G::Edge) -> Accept {
+        self(e)
     }
 }
 
 pub trait Kruskal<'a>: Basic<'a> + WithVertexProp<'a> + Sized {
-    fn kruskal_edges<I, V>(&'a self, edges: I, mut visitor: V)
+    fn kruskal_edges<I, V>(&'a self, edges: I, visitor: &mut V)
         where I: Iterator<Item = Self::Edge>,
               V: Visitor<'a, Self>
     {
         let mut ds = DisjointSet::new(self);
+        let mut num_sets = self.num_vertices();
         for e in edges {
             let (u, v) = self.endvertices(e);
-            let in_same_set = ds.in_same_set(u, v);
-            if !visitor.visit(e, in_same_set) {
-                return;
-            }
-            if !in_same_set {
+            if !ds.in_same_set(u, v) && visitor.visit(e) == Accept::Yes {
                 ds.union(u, v);
+                num_sets -= 1;
+                if num_sets == 1 {
+                    return;
+                }
             }
         }
     }
 
-    fn kruskal<T, V>(&'a self, weight: &'a EdgeProp<'a, Self, T>, visitor: V)
+    fn kruskal<T, V>(&'a self, weight: &'a EdgeProp<'a, Self, T>, visitor: &mut V)
         where T: Ord + Clone,
               V: Visitor<'a, Self>,
               Self: EdgeProperty<'a, T>
     {
-        let mut edges = self.edges().collect::<Vec<_>>();
-        edges.sort_by(|a, b| weight[*a].cmp(&weight[*b]));
+        let mut edges = self.edges().as_vec();
+        edges.sort_by(|&a, &b| weight[a].cmp(&weight[b]));
         self.kruskal_edges(edges.iter().cloned(), visitor);
     }
 
@@ -45,14 +53,12 @@ pub trait Kruskal<'a>: Basic<'a> + WithVertexProp<'a> + Sized {
         where T: Ord + Clone,
               Self: EdgeProperty<'a, T>
     {
-        let mut tree = vec![];
-        self.kruskal::<T, _>(weight, |e: Self::Edge, in_same_set: bool| {
-            if !in_same_set {
-                tree.push(e);
-            }
-            tree.len() != self.num_vertices() - 1
+        let mut edges = vec![];
+        self.kruskal(weight, &mut |e| {
+            edges.push(e);
+            Accept::Yes
         });
-        tree
+        edges
     }
 }
 
@@ -75,8 +81,8 @@ mod tests {
         // expected tree
         //      0       1       2               3
         let mut weight = g.edge_prop(0usize);
-        for (e, w) in g.edges().zip(&[1, 2, 3, 4, 5, 6, 7]) {
-            weight[e] = *w;
+        for (e, &w) in g.edges().zip(&[1, 2, 3, 4, 5, 6, 7]) {
+            weight[e] = w;
         }
         let e = g.edges().as_vec();
         assert_eq!(vec![e[0], e[1], e[2], e[4]], g.kruskal_mst(&weight));
