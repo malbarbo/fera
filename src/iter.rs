f075a1873f9cb76895ad3a4c4012f1e9b019cbed
Marco A L Barbosa <malbarbo@gmail.com>
2015-10-26 16:13:50 -0200

    New methods on iter.rs.

--- src/iter.rs
+++ src/iter.rs
@@ -1,5 +1,9 @@
-use graph::{Basic, Graph, PropEdge, Edge, Vertex};
-use ds::{Map1, IteratorExt};
+use graph::*;
+use ds::{Map1, IteratorExt, VecExt};
+
+// TODO: write tests
+// TODO: put methods that depends on g in a extension trait on Graph?
+// TODO: turn consumers methods into functions?
 
 pub trait IteratorGraphExt: Sized {
     fn endvertices<G>(self, g: &G) -> Map1<Self, G, fn(&G, Edge<G>) -> (Vertex<G>, Vertex<G>)>
@@ -9,11 +13,58 @@ pub trait IteratorGraphExt: Sized {
         self.map1(&g, G::endvertices)
     }
 
+    fn reverse_edge<G>(self, g: &G) -> Map1<Self, G, fn(&G, Edge<G>) -> Edge<G>>
+        where G: Basic,
+              Self: Iterator<Item=Edge<G>>
+    {
+        self.map1(&g, G::reverse)
+    }
+
     fn sum_edge<G>(self, w: &PropEdge<G, f32>) -> f32
         where G: Graph,
               Self: Iterator<Item=Edge<G>>,
     {
         self.fold(0.0, |acc, e| acc + w[e])
+    }
+
+    fn sorted_edge<G, T>(self, w: &PropEdge<G, T>) -> VecEdge<G>
+        where G: Graph + WithProps<T>,
+              T: PartialOrd + Clone,
+              Self: Iterator<Item=Edge<G>>,
+    {
+        self.into_vec().sorted_by(|&a, &b| w[a].partial_cmp(&w[b]).expect("partial_cmp failed"))
+    }
+
+    fn max_edge<G>(self, w: &PropEdge<G, f32>) -> Edge<G>
+        where G: Graph,
+              Self: Iterator<Item=Edge<G>>,
+    {
+        use std::f32;
+        let mut mw = f32::MIN;
+        let mut max = None;
+        for e in self {
+            if w[e] > mw {
+                mw = w[e];
+                max = Some(e);
+            }
+        }
+        max.unwrap()
+    }
+
+    fn max_edge_position<G>(self, w: &PropEdge<G, f32>) -> usize
+        where G: Graph,
+              Self: Iterator<Item=Edge<G>>,
+    {
+        use std::f32;
+        let mut mw = f32::MIN;
+        let mut pos = None;
+        for (i, e) in self.enumerate() {
+            if w[e] > mw {
+                mw = w[e];
+                pos = Some(i);
+            }
+        }
+        pos.unwrap()
     }
 }
 
