bcb79c31f97d850b1b91d48591627399f28cf767
Marco A L Barbosa <malbarbo@gmail.com>
2016-03-09 08:55:22 -0300

    Make VecProp generic on Vec and define PVecProp.

--- src/vecprop.rs
+++ src/vecprop.rs
@@ -1,27 +1,28 @@
 use graph::*;
+
+use ds::pvec::*;
+
 use std::ops::{Deref, Index, IndexMut};
+
 
 // TODO: Define a feature to disable bounds check.
 
-pub type VecPropVertex<G, T> = VecProp<VertexIndex<G>, T>;
-pub type VecPropEdge<G, T> = VecProp<EdgeIndex<G>, T>;
-
 #[derive(Clone)]
-pub struct VecProp<I, T> {
+pub struct VecProp<I, D> {
     to_index: I,
-    data: Vec<T>,
+    data: D,
 }
 
-impl<I, T> Deref for VecProp<I, T> {
-    type Target = [T];
+impl<I, D> Deref for VecProp<I, D> {
+    type Target = D;
 
     fn deref(&self) -> &Self::Target {
-        &*self.data
+        &self.data
     }
 }
 
-impl<I: ToIndex<K>, K, T> Index<K> for VecProp<I, T> {
-    type Output = T;
+impl<I: ToIndex<K>, K, D: Index<usize>> Index<K> for VecProp<I, D> {
+    type Output = D::Output;
 
     #[inline(always)]
     fn index(&self, key: K) -> &Self::Output {
@@ -29,12 +30,18 @@ impl<I: ToIndex<K>, K, T> Index<K> for VecProp<I, T> {
     }
 }
 
-impl<I: ToIndex<K>, K, T> IndexMut<K> for VecProp<I, T> {
+impl<I: ToIndex<K>, K, D: IndexMut<usize>> IndexMut<K> for VecProp<I, D> {
     #[inline(always)]
     fn index_mut(&mut self, key: K) -> &mut Self::Output {
         self.data.index_mut(self.to_index.to_index(key))
     }
 }
+
+
+// Vec
+
+pub type VecPropVertex<G, T> = VecProp<VertexIndex<G>, Vec<T>>;
+pub type VecPropEdge<G, T> = VecProp<EdgeIndex<G>, Vec<T>>;
 
 impl<G: Indices, T: Clone> PropMutVertexNew<G, T> for VecPropVertex<G, T> {
     fn new_prop_vertex(g: &G, value: T) -> Self {
@@ -53,3 +60,50 @@ impl<G: Indices, T: Clone> PropMutEdgeNew<G, T> for VecPropEdge<G, T> {
         }
     }
 }
+
+
+// PVec
+
+macro_rules! def_pvec_prop_vertex {
+    ($alias:ident, $vec:ident) => (
+        pub type $alias<G, T> = VecProp<VertexIndex<G>, $vec<T>>;
+
+        impl<G: Indices, T: 'static + Clone> PropMutVertexNew<G, T> for VecProp<VertexIndex<G>, $vec<T>> {
+            fn new_prop_vertex(g: &G, value: T) -> Self {
+                VecProp {
+                    to_index: g.prop_vertex_index(),
+                    data: $vec::with_value(value, g.num_vertices()),
+                }
+            }
+        }
+    )
+}
+
+def_pvec_prop_vertex!(PVecPropVertex, PVec);
+def_pvec_prop_vertex!(PVec0PropVertex, PVec0);
+def_pvec_prop_vertex!(PVec1PropVertex, PVec1);
+def_pvec_prop_vertex!(PVec2PropVertex, PVec2);
+def_pvec_prop_vertex!(PVec3PropVertex, PVec3);
+def_pvec_prop_vertex!(PVec4PropVertex, PVec4);
+
+macro_rules! def_pvec_prop_edge {
+    ($alias:ident, $vec:ident) => (
+        pub type $alias<G, T> = VecProp<EdgeIndex<G>, $vec<T>>;
+
+        impl<G: Indices, T: 'static + Clone> PropMutEdgeNew<G, T> for VecProp<EdgeIndex<G>, $vec<T>> {
+            fn new_prop_edge(g: &G, value: T) -> Self {
+                VecProp {
+                    to_index: g.prop_edge_index(),
+                    data: $vec::with_value(value, g.num_edges()),
+                }
+            }
+        }
+    )
+}
+
+def_pvec_prop_edge!(PVecPropEdge, PVec);
+def_pvec_prop_edge!(PVec0PropEdge, PVec0);
+def_pvec_prop_edge!(PVec1PropEdge, PVec1);
+def_pvec_prop_edge!(PVec2PropEdge, PVec2);
+def_pvec_prop_edge!(PVec3PropEdge, PVec3);
+def_pvec_prop_edge!(PVec4PropEdge, PVec4);
