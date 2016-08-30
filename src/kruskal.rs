e2aa3539b44cf1272e8d428b5c0b3a1191e0b1c8
Marco A L Barbosa <malbarbo@gmail.com>
2016-08-30 10:35:58 -0300

    Add KruskalIter

--- src/kruskal.rs
+++ src/kruskal.rs
@@ -1,8 +1,9 @@
 use graph::*;
 use fera::{IteratorExt, VecExt};
-use unionfind::WithUnionFind;
+use unionfind::{UnionFind, WithUnionFind};
+use std::vec;
 
-#[derive(PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub enum Accept {
     Yes,
     No,
@@ -23,53 +24,81 @@ impl<F, G> Visitor<G> for F
     }
 }
 
-// TODO: Allow an UnionFind as parameter, so the kruskal algorithm can be
-// executed in more than one step
+// TODO: implemets Visitor for &mut V where V: Visitor
 
-// TODO: Turn Kruskal in an Iterator
+impl<G: Graph> Visitor<G> for Accept {
+    fn visit(&mut self, _: Edge<G>) -> Accept {
+        *self
+    }
+}
 
-pub trait Kruskal: IncidenceGraph {
-    fn kruskal_with_edges<I, V>(&self, edges: I, mut visitor: V)
-        where I: Iterator<Item = Edge<Self>>,
-              V: Visitor<Self>
-    {
-        // TODO: move num_sets to UnionFind
-        let mut ds = self.new_unionfind();
-        let mut num_sets = self.num_vertices();
-        for e in edges {
-            let (u, v) = self.ends(e);
-            if !ds.in_same_set(u, v) && visitor.visit(e) == Accept::Yes {
-                ds.union(u, v);
-                num_sets -= 1;
-                if num_sets == 1 {
-                    return;
+// TODO: allow reuse of KruskalIter
+pub struct KruskalIter<'a, G: 'a + Graph, I, V> {
+    g: &'a G,
+    ds: UnionFind<G>,
+    edges: I,
+    visitor: V,
+    // TODO: move num_sets to UnionFind
+    num_sets: usize,
+}
+
+impl<'a, G, I, V> Iterator for KruskalIter<'a, G, I, V>
+    where G: 'a + Graph,
+          I: Iterator<Item = Edge<G>>,
+          V: Visitor<G>
+{
+    type Item = Edge<G>;
+
+    fn next(&mut self) -> Option<Edge<G>> {
+        if self.num_sets > 1 {
+            for e in &mut self.edges {
+                let (u, v) = self.g.ends(e);
+                if !self.ds.in_same_set(u, v) && self.visitor.visit(e) == Accept::Yes {
+                    self.ds.union(u, v);
+                    self.num_sets -= 1;
+                    return Some(e);
                 }
             }
         }
+        None
+    }
+}
+
+
+pub trait Kruskal: IncidenceGraph {
+    fn kruskal_with_edges<I, V>(&self, edges: I, visitor: V) -> KruskalIter<Self, I, V>
+        where I: Iterator<Item = Edge<Self>>,
+              V: Visitor<Self>
+    {
+        KruskalIter {
+            g: self,
+            ds: self.new_unionfind(),
+            edges: edges,
+            visitor: visitor,
+            num_sets: self.num_vertices(),
+        }
     }
 
-    fn kruskal<T, W, V>(&self, weight: &W, visitor: V)
-        where W: EdgeProp<Self, T>,
-              T: PartialOrd + Clone,
+    fn kruskal<T, W, V>(&self,
+                        weight: &W,
+                        visitor: V)
+                        -> KruskalIter<Self, vec::IntoIter<Edge<Self>>, V>
+        where W: EdgePropGet<Self, T>,
+              T: PartialOrd,
               V: Visitor<Self>
     {
         let edges = self.edges()
             .into_vec()
-            .partial_ord_sorted_by_key(|e| &weight[*e]);
+            .partial_ord_sorted_by_key(|&e| weight.get(e));
 
-        self.kruskal_with_edges(edges.into_iter(), visitor);
+        self.kruskal_with_edges(edges.into_iter(), visitor)
     }
 
-    fn kruskal_mst<T, W>(&self, weight: &W) -> VecEdge<Self>
-        where W: EdgeProp<Self, T>,
-              T: PartialOrd + Clone
+    fn kruskal_mst<T, W>(&self, weight: &W) -> KruskalIter<Self, vec::IntoIter<Edge<Self>>, Accept>
+        where W: EdgePropGet<Self, T>,
+              T: PartialOrd
     {
-        let mut tree = vec![];
-        self.kruskal(weight, |e| {
-            tree.push(e);
-            Accept::Yes
-        });
-        tree
+        self.kruskal(weight, Accept::Yes)
     }
 }
 
@@ -98,7 +127,7 @@ mod tests {
             weight[e] = w;
         }
         let e = g.edges().into_vec();
-        assert_eq!(vec![e[0], e[1], e[2], e[4]], g.kruskal_mst(&weight));
+        assert_eq!(vec![e[0], e[1], e[2], e[4]], g.kruskal_mst(&weight).into_vec());
     }
 }
 
