570531d62aa839a19824fbe33748614472cb0c4e
Marco A L Barbosa <malbarbo@gmail.com>
2017-01-23 13:58:36 -0200

    Add ConnectedComponents

--- src/components.rs
+++ src/components.rs
@@ -1,6 +1,9 @@
 use prelude::*;
 use traverse::*;
 
+use std::marker::PhantomData;
+
+// FIXME: restrict the method to appropiated graph type
 pub trait Components: Incidence {
     fn num_components(&self) -> u64
         where Self: DfsDefault
@@ -8,6 +11,14 @@ pub trait Components: Incidence {
         let mut num = 0;
         self.dfs(NumComponents(&mut num));
         num
+    }
+
+    fn connected_components(&self) -> ConnectedComponents<Self, DefaultVertexPropMut<Self, usize>>
+        where Self: DfsDefault + WithVertexProp<usize>
+    {
+        let mut cc = ConnectedComponents(self, self.vertex_prop(0));
+        self.dfs(&mut cc);
+        cc
     }
 
     fn is_connected(&self) -> bool
@@ -57,4 +68,43 @@ impl<'a, G: WithEdge> Visitor<G> for IsConnected<'a> {
 #[allow(non_snake_case)]
 pub fn NumComponents<'a>(num: &'a mut u64) -> OnDiscoverRootVertex<Count<'a>> {
     OnDiscoverRootVertex(Count(num))
+}
+
+pub struct ConnectedComponents<G, V> {
+    comp: V,
+    cur: usize,
+    _marker: PhantomData<G>,
+}
+
+#[allow(non_snake_case)]
+pub fn ConnectedComponents<G, V>(_g: &G, comp: V) -> ConnectedComponents<G, V> {
+    ConnectedComponents {
+        comp: comp,
+        cur: 0,
+        _marker: PhantomData,
+    }
+}
+
+impl<G, V> Visitor<G> for ConnectedComponents<G, V>
+    where G: WithEdge,
+          V: VertexPropMut<G, usize>
+{
+    fn discover_root_vertex(&mut self, _g: &G, _v: Vertex<G>) -> Control {
+        self.cur += 1;
+        Control::Continue
+    }
+
+    fn discover_vertex(&mut self, _g: &G, v: Vertex<G>) -> Control {
+        self.comp[v] = self.cur;
+        Control::Continue
+    }
+}
+
+impl<G, V> ConnectedComponents<G, V>
+    where G: WithEdge,
+          V: VertexPropMut<G, usize>
+{
+    pub fn is_connected(&self, u: Vertex<G>, v: Vertex<G>) -> bool {
+        self.comp[u] == self.comp[v]
+    }
 }
