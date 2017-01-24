d3b96acb7f88b97793f680d94a4ec54b13a86af6
Marco A L Barbosa <malbarbo@gmail.com>
2017-01-24 13:49:52 -0200

    Remove fera::arrays dependency

--- src/props/array.rs
+++ src/props/array.rs
@@ -1,14 +1,10 @@
 use prelude::*;
-use fera::array::{Array, VecArray};
 
 use std::ops::{Deref, Index, IndexMut};
 
 // TODO: Define a feature to disable bounds check.
-pub type ArrayVertexProp<G, A> = ArrayProp<VertexIndexProp<G>, A>;
-pub type VecVertexProp<G, T> = ArrayVertexProp<G, VecArray<T>>;
-
-pub type ArrayEdgeProp<G, A> = ArrayProp<EdgeIndexProp<G>, A>;
-pub type VecEdgeProp<G, T> = ArrayEdgeProp<G, VecArray<T>>;
+pub type VecVertexProp<G, T> = ArrayProp<VertexIndexProp<G>, Vec<T>>;
+pub type VecEdgeProp<G, T> = ArrayProp<EdgeIndexProp<G>, Vec<T>>;
 
 #[derive(Clone)]
 pub struct ArrayProp<P, D> {
@@ -37,7 +33,10 @@ impl<I, P, D> PropGet<I> for ArrayProp<P, D>
     }
 }
 
-impl<I, P: PropGet<I, Output = usize>, D: Index<usize>> Index<I> for ArrayProp<P, D> {
+impl<I, P, D> Index<I> for ArrayProp<P, D>
+    where P: PropGet<I, Output = usize>,
+          D: Index<usize>,
+{
     type Output = D::Output;
 
     #[inline(always)]
@@ -46,37 +45,38 @@ impl<I, P: PropGet<I, Output = usize>, D: Index<usize>> Index<I> for ArrayProp<P
     }
 }
 
-impl<I, P: PropGet<I, Output = usize>, D: IndexMut<usize>> IndexMut<I> for ArrayProp<P, D> {
+impl<I, P, D> IndexMut<I> for ArrayProp<P, D>
+    where P: PropGet<I, Output = usize>,
+          D: IndexMut<usize>
+{
     #[inline(always)]
     fn index_mut(&mut self, item: I) -> &mut Self::Output {
         self.data.index_mut(self.index.get(item))
     }
 }
 
-impl<A, T, G> VertexPropMutNew<G, T> for ArrayVertexProp<G, A>
-    where A: Array<T>,
-          G: VertexList + VertexIndex
+impl<T, G> VertexPropMutNew<G, T> for ArrayProp<VertexIndexProp<G>, Vec<T>>
+    where G: VertexList + VertexIndex
 {
     fn new_vertex_prop(g: &G, value: T) -> Self
         where T: Clone
     {
         ArrayProp {
             index: g.vertex_index(),
-            data: A::with_value(value, g.num_vertices()),
+            data: vec![value; g.num_vertices()]
         }
     }
 }
 
-impl<A, T, G> EdgePropMutNew<G, T> for ArrayEdgeProp<G, A>
-    where A: Array<T>,
-          G: EdgeList + EdgeIndex
+impl<T, G> EdgePropMutNew<G, T> for ArrayProp<EdgeIndexProp<G>, Vec<T>>
+    where G: EdgeList + EdgeIndex
 {
     fn new_edge_prop(g: &G, value: T) -> Self
         where T: Clone
     {
         ArrayProp {
             index: g.edge_index(),
-            data: A::with_value(value, g.num_edges()),
+            data: vec![value; g.num_edges()]
         }
     }
 }
