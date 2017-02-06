374366a0c49c1f64355db3f7d1f15de581917f13
Marco A L Barbosa <malbarbo@gmail.com>
2017-02-06 08:31:04 -0200

    Add cloned and first

--- fun/src/lib.rs
+++ fun/src/lib.rs
@@ -1,13 +1,55 @@
 use std::collections::HashSet;
 use std::hash::Hash;
+use std::iter::Cloned;
 
-pub fn vec<I: IntoIterator>(iter: I) -> Vec<I::Item>
+pub fn cloned<'a, I, T>(iter: I) -> Cloned<I::IntoIter>
+    where I: IntoIterator<Item = &'a T>,
+          T: 'a + Clone
+{
+    iter.into_iter().cloned()
+}
+
+/// Returns the first item from an iterator.
+///
+/// # Panics
+///
+/// If there is no first item.
+///
+/// # Examples
+///
+/// ```
+/// use fera_fun::first;
+///
+/// assert_eq!(&4, first(&[4, 3, 8]));
+/// assert_eq!(2, first(2..));
+/// ```
+pub fn first<I>(iter: I) -> I::Item
+    where I: IntoIterator
+{
+    iter.into_iter().next().unwrap()
+}
+
+/// Creates a Vector from a iterator.
+///
+/// ```
+/// use fera_fun::vec;
+///
+/// assert_eq!(vec![1, 2, 3], vec(1..4));
+/// ```
+pub fn vec<I>(iter: I) -> Vec<I::Item>
     where I: IntoIterator
 {
     iter.into_iter().collect()
 }
 
-pub fn set<I: IntoIterator>(iter: I) -> HashSet<I::Item>
+/// Creates a HashSet from a iterator.
+///
+/// ```
+/// use fera_fun::set;
+///
+/// assert_eq!(set(&[4, 3, 8, 3]), set(&[3, 4, 8]))
+/// ```
+pub fn set<I>(iter: I) -> HashSet<I::Item>
     where I: IntoIterator,
           I::Item: Hash + Eq
 {
