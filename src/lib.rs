221f950595d03f4853ea78f6e49f6138e534017c
Marco A L Barbosa <malbarbo@gmail.com>
2017-02-24 15:41:24 -0300

    Create a workspace and a fera crate which reexports all crates

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+macro_rules! crate_as_mod {
+    ($c: ident, $m: ident, $f: expr) => (
+        #[cfg(feature = $f)]
+        extern crate $c;
+
+        #[cfg(feature = $f)]
+        pub mod $m {
+            pub use $c::*;
+        }
+    )
+}
+
+crate_as_mod!(fera_ext, ext, "ext");
+crate_as_mod!(fera_fun, fun, "fun");
+crate_as_mod!(fera_graphs, graphs, "graphs");
+crate_as_mod!(fera_optional, optional, "optional");
+crate_as_mod!(fera_unionfind, unionfind, "unionfind");
