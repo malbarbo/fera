42a04e219134d0963159cc725811741d457a12d5
Marco A L Barbosa <malbarbo@gmail.com>
2017-03-02 13:38:34 -0300

    graph: fix benches

--- graph/benches/traverse.rs
+++ graph/benches/traverse.rs
@@ -10,51 +10,51 @@ use rand::XorShiftRng;
 use test::Bencher;
 
 fn bfs(b: &mut Bencher, g: &StaticGraph) {
-    b.iter(|| { g.bfs(OnDiscoverTreeEdge(|_| Control::Continue)); });
+    b.iter(|| { g.bfs(OnDiscoverTreeEdge(|_| Control::Continue)).run(); });
 }
 
 #[bench]
 fn bfs_complete_graph(b: &mut Bencher) {
-    let g = StaticGraph::complete(100);
+    let g = StaticGraph::new_complete(100);
     bfs(b, &g);
 }
 
 #[bench]
 fn bfs_tree(b: &mut Bencher) {
-    let g = StaticGraph::random_tree(100, XorShiftRng::new_unseeded());
+    let g = StaticGraph::new_random_tree(100, XorShiftRng::new_unseeded());
     bfs(b, &g);
 }
 
 
 fn dfs(b: &mut Bencher, g: &StaticGraph) {
-    b.iter(|| { g.dfs(OnDiscoverTreeEdge(|_| Control::Continue)); });
+    b.iter(|| { g.dfs(OnDiscoverTreeEdge(|_| Control::Continue)).run(); });
 }
 
 #[bench]
 fn dfs_complete_graph(b: &mut Bencher) {
-    let g = StaticGraph::complete(100);
+    let g = StaticGraph::new_complete(100);
     dfs(b, &g);
 }
 
 #[bench]
 fn dfs_tree(b: &mut Bencher) {
-    let g = StaticGraph::random_tree(100, XorShiftRng::new_unseeded());
+    let g = StaticGraph::new_random_tree(100, XorShiftRng::new_unseeded());
     dfs(b, &g);
 }
 
 
 fn recursive_dfs(b: &mut Bencher, g: &StaticGraph) {
-    b.iter(|| { g.recursive_dfs(OnDiscoverTreeEdge(|_| Control::Continue)); });
+    b.iter(|| { g.recursive_dfs(OnDiscoverTreeEdge(|_| Control::Continue)).run(); });
 }
 
 #[bench]
 fn recursive_dfs_complete_graph(b: &mut Bencher) {
-    let g = StaticGraph::complete(100);
+    let g = StaticGraph::new_complete(100);
     recursive_dfs(b, &g);
 }
 
 #[bench]
 fn recursive_dfs_tree(b: &mut Bencher) {
-    let g = StaticGraph::random_tree(100, XorShiftRng::new_unseeded());
+    let g = StaticGraph::new_random_tree(100, XorShiftRng::new_unseeded());
     recursive_dfs(b, &g);
 }
