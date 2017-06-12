d690506953b37233e1a630ff765fd7bd38cac112
Marco A L Barbosa <malbarbo@gmail.com>
2017-06-12 11:40:13 -0300

    graph: Add quickcheck tests for Prim and Kruskal

---
+++ graph/tests/mst.rs
@@ -0,0 +1,29 @@
+extern crate fera_graph;
+extern crate fera_fun;
+
+#[macro_use]
+extern crate quickcheck;
+
+use fera_fun::vec;
+use fera_graph::prelude::*;
+use fera_graph::kruskal::Kruskal;
+use fera_graph::prim::Prim;
+use fera_graph::trees::Trees;
+use fera_graph::arbitrary::GnConnectedWithEdgeProp;
+
+quickcheck! {
+    fn mst(x: GnConnectedWithEdgeProp<StaticGraph, u32>) -> bool {
+        let GnConnectedWithEdgeProp(g, w) = x;
+        if g.num_vertices() == 0 || g.num_edges() == 0 {
+            return true;
+        }
+        let prim = vec(g.prim(&w));
+        let w_prim: u32 = sum_prop(&w, &prim);
+        let kruskal = vec(g.kruskal_mst(&w));
+        let w_kruskal: u32 = sum_prop(&w, &kruskal);
+        assert!(g.spanning_subgraph(&prim).is_tree());
+        assert!(g.spanning_subgraph(&kruskal).is_tree());
+        assert_eq!(w_prim, w_kruskal);
+        true
+    }
+}
