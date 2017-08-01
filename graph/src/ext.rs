96ea7f31c27d58fd1395efe8374c37d509c774a3
Marco A L Barbosa <malbarbo@gmail.com>
2017-08-01 14:35:01 -0300

    graphs: add docs for ext module

--- graph/src/ext.rs
+++ graph/src/ext.rs
@@ -1,9 +1,42 @@
 //! Extension traits for std types.
+//!
+//! The traits is this module are included in the [prelude].
+//!
+//! # Examples
+//!
+//! ```
+//! extern crate fera_fun;
+//! extern crate fera_graph;
+//!
+//! use fera_graph::prelude::*;
+//! use fera_fun::vec;
+//!
+//! # fn main() {
+//! let g = CompleteGraph::new(4);
+//! let mut w = g.default_vertex_prop(0);
+//! w[0] = 1;
+//! w[1] = 2;
+//! w[2] = 3;
+//! w[3] = 0;
+//!
+//! assert_eq!(vec![3, 0, 1, 2], vec(g.vertices()).sorted_by_prop(&w));
+//! # }
+//! ```
+//!
+//! [prelude]: ../prelude/index.html
 
 use prelude::*;
 use params::IntoOwned;
 
+/// Extension trait for slices.
+///
+/// See the [module documentation] for examples.
+///
+/// [module documentation]: index.html
 pub trait GraphsSliceExt<T> {
+    /// Sort a slice using a [property].
+    ///
+    /// [property]: ../props/index.html
     fn sort_by_prop<P, K>(&mut self, prop: P)
         where P: PropGet<K>,
               for<'a> &'a T: IntoOwned<K>,
@@ -21,8 +54,15 @@ impl<T> GraphsSliceExt<T> for [T] {
     }
 }
 
-
+/// Extension trait for vectors.
+///
+/// See the [module documentation] for examples.
+///
+/// [module documentation]: index.html
 pub trait GraphsVecExt<T> {
+    /// Returns a vector sorted by a [property].
+    ///
+    /// [property]: ../props/index.html
     fn sorted_by_prop<P, K>(self, prop: P) -> Self
         where P: PropGet<K>,
               for<'a> &'a T: IntoOwned<K>,
