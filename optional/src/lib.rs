1467fbb5084cf3a02108c2f2e0a6a7e158c95075
Marco A L Barbosa <malbarbo@gmail.com>
2017-11-08 15:29:25 -0200

    optiona: add license header

--- optional/src/lib.rs
+++ optional/src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 //! An optional value trait and some implementations.
 //!
 //! There are two main uses for this:
