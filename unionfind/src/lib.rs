9413d07db14a09d3f887382f77855be87831b8d4
Marco A L Barbosa <malbarbo@gmail.com>
2017-11-08 15:30:08 -0200

    unionfind: remove copyright and add an TODO

--- unionfind/src/lib.rs
+++ unionfind/src/lib.rs
@@ -1,5 +1,3 @@
-// Copyright 2017, Marco A L Barbosa.
-//
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -98,6 +96,7 @@
 
 #![cfg_attr(feature = "cargo-clippy", allow(inline_always))]
 
+// TODO: remove fnv dependency
 extern crate fnv;
 
 use std::collections::HashMap;
