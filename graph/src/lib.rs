399c5507537b10d8dce9c256f8fe68c5a3263ecf
Marco A L Barbosa <malbarbo@gmail.com>
2018-01-02 17:03:48 -0200

    graph: version 0.1.1

--- graph/src/lib.rs
+++ graph/src/lib.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#![doc(html_root_url = "https://docs.rs/fera-graph/0.1.0/")]
+#![doc(html_root_url = "https://docs.rs/fera-graph/0.1.1/")]
 #![cfg_attr(feature = "cargo-clippy", allow(inline_always))]
 
 //! Graph data structures and algorithms.
